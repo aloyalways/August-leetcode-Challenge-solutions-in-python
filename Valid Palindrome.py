f0cb072169487060f35a1990ff0f3b3a7ac8a0b8
Aloysius Samuel <65147714+aloyalways@users.noreply.github.com>
2020-08-04 09:12:17 +0530

    Create Valid Palindrome.py

---
+++ Valid Palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s)==0 or len(s)==1:
+            return True
+        i,j=0,len(s)-1
+        while i<=j:
+            if s[i].isalnum() and s[j].isalnum():
+                if s[i].lower()==s[j].lower():
+                    i+=1
+                    j-=1
+                    continue
+                else:
+                    return False
+            if not s[i].isalnum():
+                i+=1
+            if not s[j].isalnum():
+                j-=1
+        return True
